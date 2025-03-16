5fd04051f0bb1b552d72d991b62b2afa23e9b6b6
Mitchel <noobhunt101@gmail.com>
2025-03-16 13:17:14 +0800

    Imported existing CodeBreaker project - May 2024

---
+++ Assets/Scripts/FadeMessage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeMessage : MonoBehaviour
+{
+    public static Text _text;
+
+    public float fadeSpeed = 3;
+    float decrease = 1;
+
+    private void OnEnable()
+    {
+        _text = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        if(_text.text != "")
+        {
+            decrease -= Time.deltaTime * fadeSpeed;
+            _text.color = new Color(1, 1, 1, decrease);
+        }
+        if(_text.color.a <= 0)
+        {
+            decrease = 1;
+            _text.color = Color.white;
+            _text.text = "";
+            
+        }
+    }
+}
